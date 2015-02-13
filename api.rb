adea060094fd1a1e7feef351597b7fbf49db5a15
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2015-02-12 23:42:47 -0430

    agregadas materias, secciones y estudiantes y docentes para una respuesta mas completa del servicio

--- api.rb
+++ api.rb
@@ -54,6 +54,102 @@ get '/estudiantes_y_asignaturas_en_periodo_academico/:periodo_academico_id' do
                 tipo_status_materia: "SC"
               }
             ] #materias
+          },
+          { 
+            cedula: "18132323",
+            primer_nombre: "Gonzalo",
+            segundo_nombre: "",
+            primer_apellido: "Torres",
+            segundo_apellido: "Ramírez",
+            promedio_general: 12.23,
+            eficiencia: 0.86,
+            nro_creditos_aprobados: 156,
+            materias: [
+              {
+                codigo: "6014",
+                nombre: "Probabilidad y Estadistica",
+                nombre_seccion: "C1",
+                tipo_materia_id: "O",
+                nro_creditos: 5,
+                tipo_status_materia: "SC"
+              }
+            ] #materias
+          },
+          { 
+            cedula: "14516831",
+            primer_nombre: "Adela",
+            segundo_nombre: "Aylin",
+            primer_apellido: "Mires",
+            segundo_apellido: "Dahn",
+            promedio_general: 17.5,
+            eficiencia: 0.96,
+            nro_creditos_aprobados: 142,
+            materias: [
+              {
+                codigo: "6014",
+                nombre: "Probabilidad y Estadistica",
+                nombre_seccion: "C2",
+                tipo_materia_id: "O",
+                nro_creditos: 5,
+                tipo_status_materia: "SC"
+              }
+            ] #materias
+          },
+          { 
+            cedula: "17765685",
+            primer_nombre: "Luis",
+            segundo_nombre: "Andrés",
+            primer_apellido: "Torrenegra",
+            segundo_apellido: "Armas",
+            promedio_general: 12,
+            eficiencia: 1,
+            nro_creditos_aprobados: 157,
+            materias: [
+              {
+                codigo: "6014",
+                nombre: "Probabilidad y Estadistica",
+                nombre_seccion: "C1",
+                tipo_materia_id: "O",
+                nro_creditos: 5,
+                tipo_status_materia: "SC"
+              },
+              {
+                codigo: "6221",
+                nombre: "APLIC.CON LA TEC.INTERNET",
+                tipo_materia_id: "E",
+                nombre_seccion: "C1",
+                nro_creditos: 5,
+                tipo_status_materia: "SC"
+              },
+              {
+                codigo: "6201",
+                nombre: "Algoritmos y Estructuras de Datos",
+                tipo_materia_id: "O",
+                nombre_seccion: "C1",
+                nro_creditos: 5,
+                tipo_status_materia: "SC"
+              }
+            ] #materias
+          },
+          { 
+            cedula: "21258123",
+            primer_nombre: "Manuela",
+            segundo_nombre: "Alicia",
+            primer_apellido: "Vergara",
+            segundo_apellido: "Torres",
+            promedio_general: 15.5,
+            eficiencia: 1,
+            nro_creditos_aprobados: 162,
+            materias: [
+              {
+                codigo: "6221",
+                nombre: "APLIC.CON LA TEC.INTERNET",
+                tipo_materia_id: "E",
+                nombre_seccion: "C1",
+                nro_creditos: 5,
+                tipo_status_materia: "SC"
+              }
+            ] #materias
           }
         ] #estudiantes
       }
@@ -86,7 +182,92 @@ get '/asignaturas_en_periodo_academico/:periodo_academico_id' do
                 tipo_materia_id: "O",
                 grupo_nota_id: "N20",
                 coordinador: {
-                  cedula: "122",
+		            cedula: "12243532",
+		            primer_nombre: "Luis",
+		            segundo_nombre: "Rafael",
+		            primer_apellido: "Armas",
+		            segundo_apellido: "Armas"
+                },
+                secciones: [
+					{
+		              nombre: "C1",
+		              docente: {
+		                cedula: "12243532",
+		                primer_nombre: "Luis",
+		                segundo_nombre: "Rafael",
+		                primer_apellido: "Armas",
+		                segundo_apellido: "Armas"
+		              },
+		              promedio_general: 9.8,
+		              nro_estudiantes: 25,
+		              nro_estudiantes_retirados: 2,
+		              nro_estudiantes_aprobados: 10,
+		              nro_estudiantes_equivalencia: 0,
+		              nro_estudiantes_suficiencia: 2,
+		              nro_estudiantes_repararon: 1,
+		              nro_estudiantes_aplazados: 3,
+		              tipo_status_calificacion_id: "C"
+					},
+					{
+		              nombre: "C2",
+		              docente: {
+		                cedula: "17458565",
+		                primer_nombre: "Andrés",
+		                segundo_nombre: "Rafael",
+		                primer_apellido: "Ramírez",
+		                segundo_apellido: "Armas"
+		              },
+		              promedio_general: 10.85,
+		              nro_estudiantes: 18,
+		              nro_estudiantes_retirados: 2,
+		              nro_estudiantes_aprobados: 10,
+		              nro_estudiantes_equivalencia: 0,
+		              nro_estudiantes_suficiencia: 2,
+		              nro_estudiantes_repararon: 1,
+		              nro_estudiantes_aplazados: 3,
+		              tipo_status_calificacion_id: "C"
+					}
+                ] #secciones
+              },
+              {
+                codigo: "6201",
+                nombre: "Algoritmos y Estructuras de Datos",
+                tipo_materia_id: "O",
+                grupo_nota_id: "N20",
+                coordinador: {
+                  cedula: "4259862",
+                  primer_nombre: "Luis",
+                  segundo_nombre: "",
+                  primer_apellido: "Fernández",
+                  segundo_apellido: "Armas"
+                },
+                secciones: [
+                  nombre: "C1",
+                  docente: {
+                    cedula: "4259862",
+                    primer_nombre: "Luis",
+                    segundo_nombre: "",
+                    primer_apellido: "Fernández",
+                    segundo_apellido: "Armas"
+                  },
+                  promedio_general: 9.8,
+                  nro_estudiantes: 25,
+                  nro_estudiantes_retirados: 2,
+                  nro_estudiantes_aprobados: 10,
+                  nro_estudiantes_equivalencia: 0,
+                  nro_estudiantes_suficiencia: 2,
+                  nro_estudiantes_repararon: 1,
+                  nro_estudiantes_aplazados: 3,
+                  tipo_status_calificacion_id: "C"
+                ] #secciones
+              },
+              {
+                codigo: "6221",
+                nombre: "APLIC.CON LA TEC.INTERNET",
+                tipo_materia_id: "E",
+                grupo_nota_id: "N20",
+                coordinador: {
+                  cedula: "9864565",
                   primer_nombre: "Luis",
                   segundo_nombre: "",
                   primer_apellido: "Torres",
@@ -95,11 +276,43 @@ get '/asignaturas_en_periodo_academico/:periodo_academico_id' do
                 secciones: [
                   nombre: "C1",
                   docente: {
-                    cedula: "122",
+                    cedula: "9864565",
                     primer_nombre: "Luis",
                     segundo_nombre: "",
                     primer_apellido: "Torres",
                     segundo_apellido: "Armas"
+                  },
+                  promedio_general: 9.8,
+                  nro_estudiantes: 25,
+                  nro_estudiantes_retirados: 2,
+                  nro_estudiantes_aprobados: 10,
+                  nro_estudiantes_equivalencia: 0,
+                  nro_estudiantes_suficiencia: 2,
+                  nro_estudiantes_repararon: 1,
+                  nro_estudiantes_aplazados: 3,
+                  tipo_status_calificacion_id: "C"
+                ] #secciones
+              },
+              {
+                codigo: "6536",
+                nombre: "LAB. GEN ( BASE DE DATOS)",
+                tipo_materia_id: "L",
+                grupo_nota_id: "N20",
+                coordinador: {
+                  cedula: "11255865",
+                  primer_nombre: "Félix",
+                  segundo_nombre: "",
+                  primer_apellido: "Tovar",
+                  segundo_apellido: "Armas"
+                },
+                secciones: [
+                  nombre: "C1",
+                  docente: {
+		              cedula: "11255865",
+		              primer_nombre: "Félix",
+		              segundo_nombre: "",
+		              primer_apellido: "Tovar",
+		              segundo_apellido: "Armas"
                   },
                   promedio_general: 9.8,
                   nro_estudiantes: 25,
